7f217e49bac36d6c14711aa7167bd83b7827ddc2
M1042829 <M1042829@G1ML15723>
2018-10-15 00:11:10 +0530

    files added

---
+++ src/Domain/Models/ReferralCodeTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LetsWork.Domain.Models
+{
+    public class ReferralCodeTransaction
+    {
+        public Guid ReferralTransactionId { get; set; }
+        public Guid IssuerId { get; set; }
+        public Guid BenificiaryId { get; set; }
+        public ApplicationUser User { get; set; }
+        public Guid ReferralCodeId { get; set; }
+        public ReferralCode ReferralCode { get; set; }
+        public Guid BookingId { get; set; }
+        public Booking Booking { get; set; }
+    }
+}
